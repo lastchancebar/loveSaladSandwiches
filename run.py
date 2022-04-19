b441259d3db1b958cb58e48d0080d19ebf7d0856
lastchancebar <darinaduffykelly@gmail.com>
2022-04-19 01:49:33 +0000

    update surplus sheet

--- run.py
+++ run.py
@@ -56,6 +56,16 @@ def validate_data(values):
 
     return True
 
+def update_surplus_worksheet(data):
+    """
+    update surpluss worksheet add new row with the list data provided
+    """
+    print('Updating surplus worksheet... \n')
+    surplus_worksheet = SHEET.worksheet('SURPLUS')
+    surplus_worksheet.append_row(data)
+    print('Surplus worksheet updated successfully. \n')
+
+        
 def update_sales_worksheet(data):
     """
     update sales worksheet add new row with the list data provided
@@ -64,6 +74,7 @@ def update_sales_worksheet(data):
     sales_worksheet = SHEET.worksheet('SALES')
     sales_worksheet.append_row(data)
     print('Sales worksheet updated successfully. \n')
+
 
 
 def calculate_surplus_data(sales_row):
@@ -94,7 +105,7 @@ def main():
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
     new_surplus_data = calculate_surplus_data(sales_data)
-    print(new_surplus_data)
+    update_surplus_worksheet(new_surplus_data)
 
 
 print("Welcome to Love Salad Sanwiches Data Automation")
