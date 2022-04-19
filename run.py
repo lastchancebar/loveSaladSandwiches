83329dd904b5aedfca08cba86101236b481fc2a4
lastchancebar <darinaduffykelly@gmail.com>
2022-04-19 01:15:07 +0000

    isolated the last entry in stock worksheet

--- run.py
+++ run.py
@@ -65,6 +65,28 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print('Sales worksheet updated successfully. \n')
 
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+
+def calculate_surplus_data(sales_row):
+    """
+    compare sales with stock + is waste - is sandwiches made that day
+    """
+    print('Calculating surplus data... \n')
+    stock = SHEET.worksheet('STOCK').get_all_values()
+    stock_row = stock[-1]
+    print(stock_row)
+
+
+def main():
+    """
+    run all program functions
+    """
+
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    calculate_surplus_data(sales_data)
+
+
+print("Welcome to Love Salad Sanwiches Data Automation")
+
+main()
