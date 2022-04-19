c00f4405a6d25973562babdc4da384e075fdf667
lastchancebar <darinaduffykelly@gmail.com>
2022-04-19 00:30:22 +0000

    installed while loop to keep requesting data from user until valid data is input

--- run.py
+++ run.py
@@ -17,14 +17,19 @@ def get_sales_data():
     """
     get sales data from the user
     """
-    print('Please enter sales data from the last market.')
-    print('Data should be six numbers, seperated by commas.')
-    print('Example: 10, 20, 30, 40, 50, 60\n')
 
-    data_str = input('Enter your data here:')
-    
-    sales_data = data_str.split(',')
-    validate_data(sales_data)
+    while True:
+        print('Please enter sales data from the last market.')
+        print('Data should be six numbers, seperated by commas.')
+        print('Example: 10, 20, 30, 40, 50, 60\n')
+
+        data_str = input('Enter your data here:')
+        
+        sales_data = data_str.split(',')
+
+        if validate_data(sales_data):
+            print('Data is vaild!')
+            break
 
 
 def validate_data(values):
@@ -34,11 +39,15 @@ def validate_data(values):
     or if there is not exactly six figures input.
     """
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 valued required, you provided {len(values)}"
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
+        return False
+
+    return True
 
 get_sales_data()
